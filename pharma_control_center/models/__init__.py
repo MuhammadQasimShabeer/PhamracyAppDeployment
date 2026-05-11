51cf06399d82c277fc4b3f8a4b7a12dd82b305bb
MuhammadQasimShabeer <asadkhanbloch4949@gmail.com>
2026-05-11 13:10:35 +0500

    trying to depolying the pharma control center

---
+++ pharma_control_center/models/__init__.py
@@ -0,0 +1,6 @@
+from . import pharma_medicine
+from . import pharmacy_category
+from . import pharmacy_patient
+from . import pharma_control_center
+from . import pharmacy_cart
+from . import pharmacy_interaction
