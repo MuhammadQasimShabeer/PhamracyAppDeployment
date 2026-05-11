a25a1b6bda7a37b4dca2d8f3115c332f88d7e011
MuhammadQasimShabeer <asadkhanbloch4949@gmail.com>
2026-05-11 14:25:54 +0500

    Move deployment files to pharma_control_center for Render root directory

---
+++ pharma_control_center/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Default values for environment variables
+DB_HOST=${DB_HOST:-db}
+DB_PORT=${DB_PORT:-5432}
+DB_USER=${DB_USER:-odoo}
+DB_PASSWORD=${DB_PASSWORD:-odoo}
+DB_NAME=${DB_NAME:-odoo}
+ADMIN_PASSWD=${ADMIN_PASSWD:-admin}
+
+# Run Odoo with environment variables
+exec odoo --addons-path=/mnt/extra-addons --db_host=$DB_HOST --db_port=$DB_PORT --db_user=$DB_USER --db_password=$DB_PASSWORD --db_name=$DB_NAME --xmlrpc_port=8069 --admin_passwd=$ADMIN_PASSWD
